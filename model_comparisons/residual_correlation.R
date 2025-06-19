edd60a3452a486faf72fdd19e0ef37f5c71b74bb
Peter Dunson <90647939+peterdunson@users.noreply.github.com>
2025-06-19 12:08:45 -0400

    Update residual_correlation.R

--- model_comparisons/residual_correlation.R
+++ model_comparisons/residual_correlation.R
@@ -6,8 +6,8 @@ library(rstan)
 
 # --- File paths (adjust as needed) ---
 sim_file <- "/Users/peterdunson/Desktop/Joint-Bayesian-Factor-Models/simulations/sim_scen2_1000.rds"
-fit_file <- "/Users/peterdunson/Desktop/Joint-Bayesian-Factor-Models/simulations/mgps_fit_scen2_5.rds"
-#fit_file <- "/Users/peterdunson/Desktop/Joint-Bayesian-Factor-Models/simulations/stan_tebfar_fit_scen2_5.rds"
+#fit_file <- "/Users/peterdunson/Desktop/Joint-Bayesian-Factor-Models/simulations/mgps_fit_scen2_5.rds"
+fit_file <- "/Users/peterdunson/Desktop/Joint-Bayesian-Factor-Models/simulations/stan_tebfar_fit_scen2_5.rds"
 
 # --- Load simulation & posterior ---
 sim  <- readRDS(sim_file)
@@ -83,5 +83,5 @@ plot(0:K_max, avg_abs_cor, type = "b", pch = 19,
 plot(0:K_max, max_abs_cor, type = "b", pch = 17, col = "blue",
      xlab = "Number of Factors (k)",
      ylab = "Max |Residual Correlation|",
-     main = "Baseline Sim 2")
+     main = "TEB-FAR Sim 2")
 
